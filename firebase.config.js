b201c00d2aed1ca82de62cf60788d6d850ba4e3b
git12nazneen <lipinazneen65@gmail.com>
2024-09-06 15:45:07 +0600

    Added login page

--- firebase.config.js
+++ firebase.config.js
@@ -4,7 +4,7 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
+4
 
 const firebaseConfig = {
     apiKey:import.meta.env.VITE_APIKEY,
